75c7695db63432d87edd3669b2ebd625b3490233
dmitry.ivzhenko <dmitry.ivzhenko@idfinance.com>
2022-01-07 16:13:58 +0300

    feat(part 1): initial project

---
+++ bounty-board/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+const val HERO_NAME = "Madrigal"
+
+fun main(args: Array<String>) {
+  println("Hello World!")
+
+  println(HERO_NAME)
+  var playerLevel = 5
+  println(playerLevel)
+
+  playerLevel++
+  println(playerLevel)
+}
