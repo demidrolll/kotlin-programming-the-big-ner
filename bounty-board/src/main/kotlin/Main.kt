7344b544ec311f824974b5c74e6e6b0b480b16de
dmitry.ivzhenko <dmitry.ivzhenko@idfinance.com>
2022-01-11 15:11:39 +0300

    feat(part 2)

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -4,8 +4,36 @@ fun main(args: Array<String>) {
   println("Hello World!")
 
   println(HERO_NAME)
-  var playerLevel = 5
+  var playerLevel = 4
   println(playerLevel)
+
+  val hasBefriendedBarbarians = true
+  val hasAngeredBarbarians = false
+  val playerClass = "paladin"
+  val quest = when (playerLevel) {
+    1 -> "Meet Mr. Bubbles in the land of soft things."
+    in 2..5 -> {
+      // Check whether diplomacy is an option
+      val canTalkToBarbarians = !hasAngeredBarbarians &&
+          (hasBefriendedBarbarians || playerClass == "barbarian")
+
+      if (canTalkToBarbarians) {
+        "Convince the barbarians to call off their invasion."
+      } else {
+        "Save the town from the barbarian invasions."
+      }
+    }
+    6 -> "Locate the enchanted sword."
+    7 -> "Recover the long-lost artifact of creation."
+    8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+    else -> "There are no quests right now."
+  }
+
+  println("The hero approaches the bounty board. It reads:")
+  println(quest)
+
+  println("Time passes...")
+  println("The hero returns from her quest.")
 
   playerLevel++
   println(playerLevel)
