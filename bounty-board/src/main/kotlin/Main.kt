395263201de47f1d7d95072a102fddd006597386
dmitry.ivzhenko <dmitry.ivzhenko@idfinance.com>
2022-01-19 09:26:37 +0300

    feat(part 2)

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -24,36 +24,46 @@ private fun obtainQuest(
   hasBefriendedBarbarians: Boolean = true,
   playerClass: String = "paladin",
   hasAngeredBarbarians: Boolean = false
-): String? = when (playerLevel) {
-  1 -> "Meet Mr. Bubbles in the land of soft things."
-  in 2..5 -> {
-    // Check whether diplomacy is an option
-    val canTalkToBarbarians = !hasAngeredBarbarians &&
-        (hasBefriendedBarbarians || playerClass == "barbarian")
-
-    if (canTalkToBarbarians) {
-      "Convince the barbarians to call off their invasion."
-    } else {
-      "Save the town from the barbarian invasions."
-    }
+): String? {
+  if (playerLevel <= 0) {
+    throw IllegalArgumentException("The player's level must be at least 1.")
   }
-  6 -> "Locate the enchanted sword."
-  7 -> "Recover the long-lost artifact of creation."
-  8 -> "Defeat Nogartse, bringer of death and eater of worlds."
-  else -> null
+
+  return when (playerLevel) {
+    1 -> "Meet Mr. Bubbles in the land of soft things."
+    in 2..5 -> {
+      // Check whether diplomacy is an option
+      val canTalkToBarbarians = !hasAngeredBarbarians &&
+          (hasBefriendedBarbarians || playerClass == "barbarian")
+
+      if (canTalkToBarbarians) {
+        "Convince the barbarians to call off their invasion."
+      } else {
+        "Save the town from the barbarian invasions."
+      }
+    }
+    6 -> "Locate the enchanted sword."
+    7 -> "Recover the long-lost artifact of creation."
+    8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+    else -> null
+  }
 }
 
 private fun readBountyBoard() {
-  val quest: String? = obtainQuest(playerLevel)
-  val message: String = quest
-    ?.replace("Nogartse", "xxxxxxxx")
-    ?.let { censoredQuest ->
-      """
+  val message: String = try {
+    val quest: String? = obtainQuest(playerLevel)
+    quest
+      ?.replace("Nogartse", "xxxxxxxx")
+      ?.let { censoredQuest ->
+        """
       |$HERO_NAME approaches the bounty board. It reads:
       |    "$censoredQuest"
       """.trimMargin()
-    }
-    ?: "$HERO_NAME approaches the bounty board, but it is blank."
+      }
+      ?: "$HERO_NAME approaches the bounty board, but it is blank."
+  } catch (e: Exception) {
+    "$HERO_NAME can't read, what's on then bounty board"
+  }
 
   println(message)
 }
