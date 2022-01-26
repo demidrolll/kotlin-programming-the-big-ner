7880b366f7a82643b4ee1bbec8cef001b6fa5705
dmitry.ivzhenko <dmitry.ivzhenko@idfinance.com>
2022-01-26 09:02:25 +0300

    chapter 9

--- NyetHack/src/main/kotlin/Main.kt
+++ NyetHack/src/main/kotlin/Main.kt
@@ -1,13 +1,18 @@
 fun main() {
-  narrate("A hero enters the town of Kronstadt. What is their name?") { "\u001b[33;1m$it\u001b[0m" }
-
-  val heroName = readLine()
-  require(heroName != null && heroName.isNotEmpty()) {
-    "The hero must have a name"
-  }
-
+  promptHeroName()
   changeNarratorMood()
   narrate("$heroName, ${createTitle(heroName)}, heads to the town square")
+}
+
+private fun promptHeroName(): String {
+  narrate("A hero enters the town of Kronstadt. What is their name?") { "\u001b[33;1m$it\u001b[0m" }
+
+  /*val heroName = readLine()
+  require(heroName != null && heroName.isNotEmpty()) {
+    "The hero must have a name"
+  }*/
+  println("Madrigal")
+  return "Madrigal"
 }
 
 private fun createTitle(name: String): String {
