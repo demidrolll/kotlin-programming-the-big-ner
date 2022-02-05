adf98e98c18098f3dda5279824115f42f94f09ed
dmitry.ivzhenko <dmitry.ivzhenko@idfinance.com>
2022-02-05 21:06:39 +0300

    challenge

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -10,15 +10,31 @@ private val menuData = File("data/tavern-menu-data.txt")
   .readText()
   .split("\n")
 
+data class MenuItem(
+  val type: String,
+  val name: String,
+  val price: String
+)
+
 private val menuItems = List(menuData.size) {
-  val (_, name, _) = menuData[it].split(",")
-  name
+  val (type, name, price) = menuData[it].split(",")
+  MenuItem(type, name, price.trim())
 }
 
 fun visitTavern() {
   narrate("$heroName enters $TAVERN_NAME")
   narrate("There are several items for sale:")
-  narrate(menuItems.joinToString())
+  val maxName = menuItems.stream().map { it.name.length }.max(Int::compareTo).orElseThrow() + 3
+  val maxPrice = menuItems.stream().map { it.price.length }.max(Int::compareTo).orElseThrow()
+  var currentType = ""
+  menuItems.sortedBy { it.type }.forEach {
+    if (currentType != it.type) {
+      currentType = it.type
+      val typeName = "~[$currentType]~"
+      println(typeName.padStart(((maxName + maxPrice) / 2) + (typeName.length / 2)))
+    }
+    println(it.name.padEnd(maxName, '.') + it.price.padStart(maxPrice, '.'))
+  }
 
   val patrons = mutableSetOf<String>()
   while (patrons.size < 10) {
@@ -28,7 +44,7 @@ fun visitTavern() {
   narrate("$heroName sees several patrons in the tavern:")
   narrate(patrons.joinToString())
   repeat(3) {
-    placeOrder(patrons.random(), menuItems.random())
+    placeOrder(patrons.random(), menuItems.random().name)
   }
 }
 
